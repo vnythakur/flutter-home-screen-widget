0c460e09cd5d8ddbd261d69327e89ec5b879361b
Vinay Thakur <thakur.vny92@gmail.com>
2024-01-06 19:27:57 +0530

    Basic iOS Widget added

---
+++ ios/NewsWidgets/NewsWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewsWidgetsBundle.swift
+//  NewsWidgets
+//
+//  Created by Vinay Thakur on 06/01/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidgets()
+    }
+}
